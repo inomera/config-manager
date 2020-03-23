e259b66cdd486ba44a9ea69f3a64349fd6c6d0ec
mrscoa <melek.uzun@inomera.com>
2020-03-23 12:35:03 +0300

    Refactor onChange method
    resolves: VDFPMP-1148

--- config-manager/src/main/java/com/inomera/telco/commons/config/change/ConfigurationChangeListener.java
+++ config-manager/src/main/java/com/inomera/telco/commons/config/change/ConfigurationChangeListener.java
@@ -4,6 +4,6 @@ package com.inomera.telco.commons.config.change;
  * @author Melek UZUN
  */
 public interface ConfigurationChangeListener {
-    void onChange(String oldValue, String newValue);
+    void onChange(String configKey, String oldValue, String newValue);
 
 }
