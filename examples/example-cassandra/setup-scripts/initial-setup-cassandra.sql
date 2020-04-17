3ab11ffe67f7651608b6f3dd1f39d0e58860742b
Serdar Kuzucu <serdarkuzucu13@gmail.com>
2020-04-17 10:51:47 +0300

    Make cassandra beans conditional to allow user project use its own cassandra

--- examples/example-cassandra/setup-scripts/initial-setup-cassandra.sql
+++ examples/example-cassandra/setup-scripts/initial-setup-cassandra.sql
@@ -1,14 +1,24 @@
-CREATE TABLE mps.app_settings(" +
-                    "   application text," +
-                    "   key text," +
-                    "   value text," +
-                    "   PRIMARY KEY (application, key));
+CREATE KEYSPACE IF NOT EXISTS example_cassandra WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};
+
+CREATE TABLE example_cassandra.app_settings
+(
+    application text,
+    key         text,
+    value       text,
+    PRIMARY KEY (application, key)
+);
 
 
-insert into mps.app_settings(application,key,value) values ('application', 'first-key', 'first-value');
-insert into mps.app_settings (application,key,value) values ('application', 'second-key', 'second-value');
-insert into mps.app_settings (application,key,value) values ('application', 'third-key', 'third-value');
-insert into mps.app_settings (application,key,value) values ('application', 'fifth-key', 'fifth-value');
+insert into example_cassandra.app_settings(application, key, value)
+values ('application', 'first-key', 'first-value');
+insert into example_cassandra.app_settings (application, key, value)
+values ('application', 'second-key', 'second-value');
+insert into example_cassandra.app_settings (application, key, value)
+values ('application', 'third-key', 'third-value');
+insert into example_cassandra.app_settings (application, key, value)
+values ('application', 'fifth-key', 'fifth-value');
 
-insert into mps.app_settings (application,key,value) values ('charging', 'second-key', 'second-value-2');
-insert into mps.app_settings (application,key,value) values ('charging', 'first-key', 'first-value-2');
+insert into example_cassandra.app_settings (application, key, value)
+values ('charging', 'second-key', 'second-value-2');
+insert into example_cassandra.app_settings (application, key, value)
+values ('charging', 'first-key', 'first-value-2');
